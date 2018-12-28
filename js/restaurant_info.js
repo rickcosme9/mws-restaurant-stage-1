7685d86dcfb2774014a6468f0962672f5e9e7c48
Rick Cosme <richard.cosme@demandmedia.com>
2018-12-28 09:03:36 -0800

    updated mapbox token

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiYXVkaXN0ZXIiLCJhIjoiY2pxN3FocjFsMDcyZDN4bzBtZzdhdGIycyJ9.T5wFWRz541ks3pKUEh8BRg',
+        mapboxToken: 'pk.eyJ1IjoiYXVkaXN0ZXIiLCJhIjoiY2pxN3F2bDZrMnQ4dDRjczdpdTNmdjJ4MSJ9.Tonr9CUHR6Tyw7MOZDj_rw',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
