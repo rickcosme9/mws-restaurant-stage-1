5a9f46a422449a28107e2a239b4454e225f0967f
Rick Cosme <44131509+rickcosme9@users.noreply.github.com>
2019-01-06 14:53:56 -0800

    fix db url

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -13,7 +13,7 @@ class DBHelper {
     //return `http://localhost:${port}/data/restaurants.json`;
 
     //for github
-    return `https://rickcosme9.github.io/restaurant-review/data/restaurants.json`;
+    return `./data/restaurants.json`;
   }
 
   /**
